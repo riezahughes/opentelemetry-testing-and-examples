b8aaae0969d9cc105304360dfd634e2d1b812310
dan ruxton <dan@tantalus-troupe.com>
2023-10-12 09:06:15 +0200

    testing telemetry stack

---
+++ rest-app/meter.js
@@ -0,0 +1,30 @@
+//meter.js
+
+"use strict";
+
+const { Resource } = require("@opentelemetry/resources");
+const { metrics } = require("@opentelemetry/api");
+const {
+  OTLPMetricExporter,
+} = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const {
+  MeterProvider,
+  PeriodicExportingMetricReader,
+} = require("@opentelemetry/sdk-metrics");
+
+const meterProvider = new MeterProvider({
+  resource: new Resource({ "service.name": "my-express-app" }),
+});
+
+const metricExporter = new OTLPMetricExporter({
+  url: "http://otel-collector:4317",
+});
+
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricExporter,
+  exportIntervalMillis: 60000,
+});
+
+meterProvider.addMetricReader(metricReader);
+
+metrics.setGlobalMeterProvider(meterProvider);
